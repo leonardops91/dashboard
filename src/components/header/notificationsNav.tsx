c26e639aa0214fa6253cd8d23a807a3f385de148
Leonardo Souza <leonardops91@gmail.com>
2023-07-07 16:22:31 -0300

    feat: funções dos botões

--- src/components/header/notificationsNav.tsx
+++ src/components/header/notificationsNav.tsx
@@ -1,19 +1,64 @@
-import { HStack, Icon } from "@chakra-ui/react";
+import { Avatar, Button, Flex, HStack, Icon, Link, Popover, PopoverContent, PopoverTrigger, Portal, Text } from "@chakra-ui/react";
 import { RiNotification4Line, RiUserAddLine } from "react-icons/ri";
+import { NavLink } from "../sidebar/navLink";
 
 export function NotificationNav() {
+
+
+  function handleOpenNotifications() {
+
+  }
   return (
     <HStack
-      spacing={['6', '8']}
-      mx={['6','8']}
-      pr={['6','8']}
+      spacing={["6", "8"]}
+      mx={["6", "8"]}
+      pr={["6", "8"]}
       py='1'
       color='gray.300'
       borderRightWidth={1}
       borderColor='gray.700'
     >
-      <Icon as={RiNotification4Line} fontSize={20} />
-      <Icon as={RiUserAddLine} fontSize={20} />
+      <Popover>
+        <PopoverTrigger>
+          <Button w='0' bg='none' color='gray.300' _hover={{ bg: "none", color:'gray.50' }}>
+            <NavLink
+              as={Flex}
+              href='#'
+              onClick={handleOpenNotifications}
+              alignItems='center'
+              justifyContent='center'
+              transition='all .2s'
+              _hover={{ color: "gray.50" }}
+            >
+              <Icon as={RiNotification4Line} fontSize={20} />
+            </NavLink>
+          </Button>
+        </PopoverTrigger>
+        <Portal>
+          <PopoverContent
+            p={5}
+            w='fit-content'
+            bg='gray.700'
+            border='none'
+            alignItems='center'
+            transitionProperty={"all"}
+            transitionDuration={".1s"}
+            cursor='pointer'
+          >
+            <Text>Nenhuma notificação</Text>
+          </PopoverContent>
+        </Portal>
+      </Popover>
+
+      <NavLink
+        href='../users/create'
+        alignItems='center'
+        justifyContent='center'
+        transition='all .2s'
+        _hover={{ color: "gray.50" }}
+      >
+        <Icon as={RiUserAddLine} fontSize={20} />
+      </NavLink>
     </HStack>
   );
 }
--- src/components/sidebar/navLink.tsx
+++ src/components/sidebar/navLink.tsx
@@ -1,10 +1,10 @@
 import { Icon, Text, Link as LinkBase, LinkProps as LinkBaseProps } from "@chakra-ui/react";
-import { ElementType } from "react";
+import { ElementType, ReactNode } from "react";
 import { useLocation } from "react-router-dom";
 
 interface NavLinkProps extends LinkBaseProps{
-    children: string,
-    icon: ElementType,
+    children: string | ReactNode
+    icon?: ElementType,
     href: string
 }
 
@@ -12,11 +12,23 @@ export function NavLink({ children, icon, href, ...rest}: NavLinkProps){
     const location = useLocation()
 
     return (
-        <LinkBase href={href} display='flex' alignItems='center' color={location.pathname.startsWith(href) ? 'purple.500' : ''} {...rest}>
-          <Icon as={icon} fontSize='20' />
-          <Text ml='4' fontWeight='medium'>
-            {children}
-          </Text>
-        </LinkBase>
+      <LinkBase
+        href={href}
+        display='flex'
+        alignItems='center'
+        color={location.pathname.startsWith(href) ? "purple.500" : ""}
+        {...rest}
+      >
+        {icon ? (
+          <>
+            <Icon as={icon} fontSize='20' />
+            <Text ml='4' fontWeight='medium'>
+              {children}
+            </Text>
+          </>
+        ) : (
+          <>{children}</>
+        )}
+      </LinkBase>
     );
 }
